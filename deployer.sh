15d275a17badcace9cd4a835715e45f59e739d43
Muhamad Jamil <rezaramdhani461@gmail.com>
2024-11-14 11:55:02 +0700

    ci: change port

--- deployer.sh
+++ deployer.sh
@@ -4,4 +4,4 @@ docker container stop aethereal-node
 docker container rm aethereal-node
 docker image rm aethereal-node
 docker build -t aethereal-node .
-docker run -d -p 4000:3001 --restart unless-stopped --name aethereal-node aethereal-node
+docker run -d -p 4001:80 --restart unless-stopped --name aethereal-node aethereal-node
